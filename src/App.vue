08fd2923b45477c7390c6a7ced46bab93656b24f
glrose <garrett.rose03@gmail.com>
2020-08-05 22:22:56 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="about">About</router-link></li>
+    </ul>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//registering our components so we can embed them like "<> </>"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -23,6 +25,38 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+/* li {
+  list-style: none;
+  margin: 60px;
+  display: inline;
+} */
+
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>Welcome to my website!</h1>
+    <p>My name is Garrett Rose. I am a self taught web developer from Charlotte, North Carolina. I am for hire.</p>
+    <img src="../pics/profilePic.jpg" alt="vue logo"><br>
+    <a href="https://github.com/GLRose">Github</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+img {
+  border: 5px solid #555;
+}
+</style>
